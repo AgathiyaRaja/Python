06e77634991607b37f7b2539a3866414a8227b2f
Agathiya <agathiya7s@gmail.com>
2020-04-05 14:12:32 +0530

    Add files via upload

---
+++ exe3.py
@@ -0,0 +1,13 @@
+print("Hens", 25 + 30 / 6) #Count no.of Hens
+print("Roosters", 100 - 25 * 3 % 4) #Count no.of Roosters
+print("Now I will count the eggs:")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) #Egg counting   / completion of task 1
+print("Is it true that 3 + 2 < 5 - 7?") #Finding T/F       /   Task 2
+print(3 + 2 < 5 - 7)
+print("What is 3 + 2?", 3 + 2) #Logical explanation
+print("What is 5 - 7?", 5 - 7)
+print("Oh, that's why it's False.") #Completion of task 2
+print("How about some more.") #Task 3
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2) #completion of task 3
