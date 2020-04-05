9f39f7eaaea79cf1e3b48d2e80196e5be0ebac6f
Agathiya <agathiya7s@gmail.com>
2020-04-05 16:44:07 +0530

    Add files via upload
    Debugging

---
+++ exxe4.1.py
@@ -0,0 +1,15 @@
+my_name = 'Dhiya'
+my_age = 22 # not a lie
+my_height = 157 # cm (approx)
+my_weight = 55 # kg   maybe a lie
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+print(f"Let's talk about {my_name}")
+print(f"She's {my_height} cm tall.")
+print(f"She's {my_weight} kg heavy.")
+print("Actually that's not too heavy.")
+print(f"She's got {my_eyes} eyes and {my_hair} hair.")
+print(f"Her teeth are usually {my_teeth} depending on the coffee.")
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get ")
+print(my_age + my_height + my_weight)
